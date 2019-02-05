6fdce91f92a5a08b89b376b990c3496f1332039e
Ashley Kuttler <ashleykuttler@gmail.com>
2019-02-05 09:07:17 -0600

    re-defined monthly_changes back to list, appending dates to separte list

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,7 +13,8 @@ with open(budget_data_path, newline="") as budget_data_file:
     total_profit_loss = 0
     total_changes = 0
     previous_row = 0 # !!instead of zero how do i define the first row change as null or none
-    monthly_changes = {}
+    monthly_changes = []
+    date_list = []
 
     for row in budget_data_reader:
 # count rows in file as total months
@@ -23,17 +24,19 @@ with open(budget_data_path, newline="") as budget_data_file:
 # average change over entire period
     #subtract previous row profit/loss from current row profit/loss
         change = int(row[1]) - int(previous_row)
-    #!! append change and date to dictionary monthly changes
-        #monthly_changes[change].append(int(change)) 
-        #monthly_changes[Date].append(row[0]) 
+#append change and date to lists
+        monthly_changes.append(int(change)) 
+        date_list.append(row[0])
     # total changes
         total_changes += int(change)
     # divide total changes by total months  
         avg_change = round((total_changes/total_months),2)                
 # max profit
-        greatest_increase = max(monthly_changes) # !! Call Max change and corresponding date from dic
+        greatest_increase = max(monthly_changes)
+# !! Callcorresponding date from list
 # min profit(loss)
         greatest_decrease = min(monthly_changes)
+# !! Callcorresponding date from list
 #set previous row to current row at the end of each loop
         previous_row = row[1]
 # print analysis results
@@ -46,6 +49,7 @@ with open(budget_data_path, newline="") as budget_data_file:
     print("Greatest Decrease in Profits: $"+ str(greatest_decrease)+" on ")
     print(monthly_changes)
     print(total_changes)
+    print(date_list)
    
 
    
